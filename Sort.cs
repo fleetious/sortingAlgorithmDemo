4fad6b9e2e3f9f0f8b8d872b1e9042379a1cb2b2
Enzo Liu <kiddies.tricks_6g@icloud.com>
2025-02-02 12:00:35 -0800

    Update Sort.cs

--- Sort.cs
+++ Sort.cs
@@ -2,44 +2,96 @@ namespace SortingAlgorithmDemo;
 
 public class Sort
 {
-    public static bool IsSorted(int[] array)
+    public static (int, int) FindSmallestValue(int[] array, int startSearchFromIndex)
     {
-        // handle cases where array size is less than 2
-        if(array.Length < 2 && array[0] < array[1]) return true;
-        
-        for(int i = 1; i < array.Length; i++)
+        int smallestValue = array[startSearchFromIndex];
+        int indexOfSmallestValue = startSearchFromIndex;
+
+        for(int i = startSearchFromIndex + 1; i < array.Length; i++)
         {
-            if(array[i - 1] > array[i]) return false;
+            if (array[i] < smallestValue)
+            {
+                smallestValue = array[i];
+                indexOfSmallestValue = i;
+            }
         }
-        
-        return true;
+
+        return (smallestValue, indexOfSmallestValue);
     }
     
     public static int[] BubbleSort(int[] array)
     {
-        int[] sorted = array;
-        int allSortedFromIndex = array.Length; // initialized as entire array is not sorted
+        int allSortedFromIndex = array.Length; // initialized as entire array is not array
         int currentStartValue = array[0];
+        bool hasSwapOccured = true;
 
-        while(!IsSorted(sorted))
+        while(hasSwapOccured)
         {
+            hasSwapOccured = false;
+
             for(int i = 1; i < allSortedFromIndex; i++)
             {
-                if(sorted[i - 1] > sorted[i])
+                if(array[i - 1] > array[i])
                 {
-                    int temp = sorted[i];
-                    sorted[i] = sorted[i - 1];
-                    sorted[i - 1] = temp;
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+
+                    hasSwapOccured = true;
                 }
             }
 
-            if(sorted[allSortedFromIndex - 1] == currentStartValue)
+            if(array[allSortedFromIndex - 1] == currentStartValue)
             {
-                currentStartValue = sorted[0];
+                currentStartValue = array[0];
                 allSortedFromIndex--;
             }
+
         }
         
-        return sorted;
+        return array;
+    }
+
+    public static int[] SelectionSort(int[] array)
+    {
+        int allSortedBeforeIndex = 0;
+
+        while(allSortedBeforeIndex < array.Length)
+        {
+            int temp = array[allSortedBeforeIndex];
+            (int value, int index) = FindSmallestValue(array, allSortedBeforeIndex);
+
+            array[index] = temp;
+            array[allSortedBeforeIndex] = value;
+
+            allSortedBeforeIndex++;
+        }
+
+        return array;
+    }
+
+    public static int[] InsertionSort(int[] array)
+    {
+        int allSortedBeforeIndex = 1;
+        bool hasSwapOccurred = true;
+
+        while(hasSwapOccurred)
+        {
+            hasSwapOccurred = false;
+
+            for(int i = allSortedBeforeIndex - 1; i > 0; i--)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    
+
+                    hasSwapOccurred = true;
+                }
+            }
+
+            allSortedBeforeIndex--;
+        }
+
+        return array;
     }
 }
